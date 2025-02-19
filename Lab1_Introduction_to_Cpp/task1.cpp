4af5d8a95cf6b63553033a843cf7e3826fd6b352
abanoub-refaat <abanoubref3at18@gmail.com>
2025-02-19 11:20:44 +0200

    add task 1

---
+++ Lab1_Introduction_to_Cpp/task1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string tarek;
+    cout << "Enter a string ya kebeer";
+    getline(cin, tarek);
+
+    int count = 0;
+
+    for (int i = 0; i < tarek.length(); i++)
+    {
+        switch (tarek[i])
+        {
+        case 'a':
+            count++;
+            break;
+        case 'i':
+            count++;
+            break;
+        case 'e':
+            count++;
+            break;
+        case 'u':
+            count++;
+            break;
+        case 'o':
+            count++;
+            break;
+        default:
+            break;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
