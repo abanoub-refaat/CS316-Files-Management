5384bacb250836f8c10f18b90d5b2d2c5ea93366
abanoub-refaat <abanoubref3at18@gmail.com>
2025-03-11 19:02:50 +0200

    add Lab3 File I/O task: create a program to input names and job titles

---
+++ Lab3_FileIO/task5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    bool flag = 1;
+
+    ofstream myfile;
+    myfile.open("example.txt");
+
+    if (myfile.is_open())
+    {
+        while (flag)
+        {
+            string fname;
+            string lname;
+            string job;
+
+            cout << "Enter your first name: ";
+            cin >> fname;
+            myfile << " " << fname;
+
+            cout << "Enter your last name: ";
+            cin >> lname;
+            myfile << " " << lname;
+
+            cout << "Enter your job title: ";
+            cin >> job;
+            myfile << " " << job << endl;
+
+            cout << "Do you want to continue?(1/0) ";
+            cin >> flag;
+        }
+        myfile.close();
+    }
+    else
+        cout << "Unable to open flie";
+
+        return 0;
+}
