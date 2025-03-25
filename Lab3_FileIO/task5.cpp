119274457ad41c181874fa49b32e66934eb29442
abanoub-refaat <abanoubref3at18@gmail.com>
2025-03-25 16:34:09 +0200

    add Lab3 task5: implement line count functionality for list.txt

--- Lab3_FileIO/task5.cpp
+++ Lab3_FileIO/task5.cpp
@@ -37,7 +37,30 @@ int main()
         myfile.close();
     }
     else
+    {
         cout << "Unable to open flie";
+        return 1;
+    }
+
+    ifstream readFile("list.txt");
+    int count = 0;
+    string line;
+
+    if (readFile.is_open())
+    {
+        while (getline(readFile, line))
+        {
+            if (!line.empty())
+                count++;
+        }
+        readFile.close();
+        cout << "\nThere are " << count << " lines in the file.\n";
+    }
+    else
+    {
+        cout << "Unable to open file";
+        return 1;
+    }
 
     return 0;
 }
